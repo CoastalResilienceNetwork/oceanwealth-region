fd54f9f246ac53add30b1ed2d8ed0162704e713c
Zach Ferdana <zferdana@tnc.org>
2015-05-20 12:01:01 -0700

    updated layer selector apps for ecosystem services

---
+++ plugins/blue_carbon/main.js
@@ -0,0 +1,18 @@
+define([
+    "dojo/_base/declare",
+    "plugins/layer_selector/main",
+    "dojo/text!./layers.json"],
+    function (declare,
+              LayerSelectorPlugin,
+              layerSourcesJson) {
+        return declare(LayerSelectorPlugin, {
+            toolbarName: "Blue Carbon",
+            fullName: "Configure and control layers to be overlayed on the base map.",
+			infoGraphic: "",
+
+            getLayersJson: function() {
+                return layerSourcesJson;
+            }
+        });
+    }
+);
